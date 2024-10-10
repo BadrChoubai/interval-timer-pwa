4a6ea72f44d421ed478f07ce435ef9770a7409fd
Badr <choubai.badr@protonmail.com>
2024-10-10 16:40:08 -0600

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useTimeAgo } from '@vueuse/core'
+import ReloadPrompt from './components/ReloadPrompt.vue'
+
+const date = '__DATE__'
+const timeAgo = useTimeAgo(date)
+</script>
+
+<template>
+  <header>
+    <h1>Intervals</h1>
+  </header>
+  <main>
+    Under Construction
+  </main>
+  <footer>
+    <div>Built at: {{ date }} ({{ timeAgo }})</div>
+  </footer>
+  <ReloadPrompt />
+</template>
---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import {fileURLToPath, URL} from 'node:url'
+import {VitePWA} from 'vite-plugin-pwa'
+import replace from '@rollup/plugin-replace'
+
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        VitePWA({
+                registerType: 'autoUpdate', devOptions: {
+                    enabled: true,
+                },
+                injectRegister: 'auto',
+                includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+                manifest: {
+                    name: 'Intervals.timer',
+                    short_name: 'Intervals',
+                    description: 'An Interval Timer for Workouts',
+                    theme_color: '#ffffff',
+                    icons: [
+                        {
+                            src: 'pwa-192x192.png',
+                            sizes: '192x192',
+                            type: 'image/png'
+                        },
+                        {
+                            src: 'pwa-512x512.png',
+                            sizes: '512x512',
+                            type: 'image/png'
+                        }
+                    ]
+                }
+        }
+        ),
+        replace({
+            __DATE__: new Date().toISOString(),
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
+})
