e2a9d8e891b7075dd2663895b68909e64d8021f1
Badr <choubai.badr@protonmail.com>
2024-10-10 16:55:26 -0600

    deploy on Netlify

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,7 @@ export default defineConfig({
     plugins: [
         vue(),
         VitePWA({
-                registerType: 'autoUpdate', devOptions: {
-                    enabled: true,
-                },
+                registerType: 'autoUpdate',
                 injectRegister: 'auto',
                 includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
                 manifest: {
